90ea00575cb849dc318ce11c91ccc2f21bd916af
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2021-10-13 22:07:42 -0500

    add header and footer to main layout

---
+++ src/components/Footer/MadeByFoot.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text-muted m-0 pt-1 pl-3 text-left text-sm-right">
+    <small>
+      Made by
+      <a href="/" target="_blank"> Glory Rojas </a>
+      with love
+    </small>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'MadeByFoot'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,25 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.styl'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faSkull,
+    faCrown,
+    faDungeon,
+    faHatWizard,
+    faHammer,
+    faGem,
+    faVuejs,
+    faBootstrap,
+    faFontAwesome,
+    faGithub,
+    faBattleNet
+)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
