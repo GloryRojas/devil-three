f99a98fcd24e86438cfd4f6b0cd968e071b8c28b
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2021-10-13 22:13:57 -0500

    group plugins on different files

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './plugins/bootstrapVue'
+import './plugins/fontAwesome'
 import './assets/css/main.styl'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
-import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(
-    faSkull,
-    faCrown,
-    faDungeon,
-    faHatWizard,
-    faHammer,
-    faGem,
-    faVuejs,
-    faBootstrap,
-    faFontAwesome,
-    faGithub,
-    faBattleNet
-)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
