c4a48e6d5e753fd4940d5aafd4648900b73e6634
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2021-10-25 22:36:53 -0500

    add redirect to profile details

--- src/views/Profile/MainBlock/TopHeroes/TopHero.vue
+++ src/views/Profile/MainBlock/TopHeroes/TopHero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hero-portrait-wrapper mb-5 mb-sm-0">
+  <div class="hero-portrait-wrapper mb-5 mb-sm-0 hover-cursor-pointer" @click="goToHero(hero.id)">
     <div class="bg-secondary d-flex justify-content-center p-3 p-sm-0">
       <div :class="heroClass"></div>
     </div>
@@ -26,8 +26,11 @@
 </template>
 
 <script>
+import goToHero from "../../../../mixins/goToHero";
+
 export default {
   name: 'TopHero',
+  mixins: [goToHero],
   props: {
     hero: {
       type: Object,
