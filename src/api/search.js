b0aaffa6b3f691ae90ae57a751cf8410b16931ce
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2021-10-26 10:58:58 -0500

    add functions to get data of hero and hero items

--- src/api/search.js
+++ src/api/search.js
@@ -15,9 +15,37 @@ function getApiAccount ({region, account}) {
         locale,
     };
 
-    return axios.get(API_URL,{params});
+    return axios.get(API_URL,{ params });
+}
+
+function getApiHero ({region, account, heroId}) {
+    const resource = `d3/profile/${account}/hero/${heroId}`;
+    const API_URL = `${protocol}${region}${host}${resource}`;
+    const locale = locales[region];
+
+    const params = {
+        'access_token' : store.state.oauth.accessToken,
+        locale,
+    }
+
+    return axios.get(API_URL, { params });
+}
+
+function getApiDetailHeroItems ({ region, account, heroId }) {
+    const resource = `d3/profile/${account}/hero/${heroId}/items`;
+    const API_URL = `${protocol}${region}${host}${resource}`;
+    const locale = locales[region];
+
+    const params = {
+        'access_token' : store.state.oauth.accessToken,
+        locale,
+    }
+
+    return axios.get(API_URL, { params });
 }
 
 export {
     getApiAccount,
+    getApiHero,
+    getApiDetailHeroItems
 }
--- src/views/Hero/Index.vue
+++ src/views/Hero/Index.vue
@@ -1,9 +1,66 @@
 <template>
-  <h1>Hero page</h1>
+  <div>
+    <h1>Hero page</h1>
+    <BaseLoading v-if="isLoadingHero" />
+    <BaseLoading v-if="isLoadingItems" />
+  </div>
 </template>
 
 <script>
+import setError from "../../mixins/setError";
+import BaseLoading from "../../components/BaseLoading";
+import { getApiHero, getApiDetailHeroItems } from "../../api/search";
+
 export default {
-  name: 'HeroView'
+  name: 'HeroView',
+  mixins: [setError],
+  components: { BaseLoading },
+  data() {
+    return {
+      isLoadingHero: true,
+      isLoadingItems: true,
+      hero: null,
+      items: null,
+    }
+  },
+  computed: {},
+  created() {
+    this.isLoadingHero = true;
+    this.isLoadingItems = true;
+    const { region, battleTag: account, heroId } = this.$route.params;
+
+    getApiHero({region, account, heroId})
+    .then((data) => {
+      this.hero = data;
+    })
+    .catch((err) => {
+      this.hero = null;
+      const errObj = {
+        routeParams: this.$route.params,
+        message: err.message,
+      }
+      if(err.response) {
+        errObj.data = err.response.data
+        errObj.status = err.response.status
+      }
+      this.setApiErr(errObj);
+      this.$router.push({ name: 'Error' })
+    })
+    .finally(() => {
+      this.isLoadingHero = false;
+    })
+
+    getApiDetailHeroItems({ region, account, heroId })
+      .then(({ data }) => {
+        this.items = data
+      })
+      .catch((err) => {
+        this.items = null
+        console.log(err)
+      })
+      .finally(() => {
+        this.isLoadingItems = false
+      })
+  },
 }
 </script>
