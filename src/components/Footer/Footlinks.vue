12d0a82c0aecb2da6a92dc4367e0370a7ddc0405
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2021-10-26 22:20:09 -0500

    fix redirect on footer links

--- src/components/Footer/Footlinks.vue
+++ src/components/Footer/Footlinks.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <b-nav small align="start">
-      <b-nav-item to="{name: 'Home'}">
+      <b-nav-item  to="/home">
         Home
       </b-nav-item>
-      <b-nav-item to="{name: 'About'}">
+      <b-nav-item to="/about">
         About
       </b-nav-item>
-      <b-nav-item href="/" target="_blank">
+      <b-nav-item href="https://github.com/GloryRojas" target="_blank">
         Github
       </b-nav-item>
     </b-nav>
--- src/components/Footer/MadeByFoot.vue
+++ src/components/Footer/MadeByFoot.vue
@@ -2,7 +2,7 @@
   <p class="text-muted m-0 pt-1 pl-3 text-left text-sm-right">
     <small>
       Made by
-      <a href="/" target="_blank"> Glory Rojas </a>
+      <a href="https://glory-rojas-c4c53lov1.vercel.app/" target="_blank"> Glory Rojas </a>
       with love
     </small>
   </p>
